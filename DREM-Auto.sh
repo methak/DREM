cfd3de303026dbbc44500aac560b21e9dc7a1772
methak <saad6mzed@gmail.com>
2017-05-24 20:23:43 +0300

    Update and rename TSHAKE-Auto.sh to DREM-Auto.sh

--- DREM-Auto.sh
+++ DREM-Auto.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 COUNTER=1
 while(true) do
-./TSHAKE.sh
+./DREM.sh
 curl "https://api.telegram.org/bot[362136053:AAGkJVHSOaAHFADlrQmKDn28AGLDNIbEA5c]/sendmessage" -F "chat_id=67369633" -F "text=#NEWCRASH-#TeleBeyond-Reloaded-${COUNTER}-times"
 let COUNTER=COUNTER+1 
 done
